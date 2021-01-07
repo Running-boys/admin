a01794bf6202dd7b43c78dc23f5201b0a2646552
易磊 <469235606@qq.com>
2021-01-07 21:42:04 +0800

    a

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,17 @@
+// 驼峰转换下划线
+export const toSQLLine = (str) => {
+    if (str == "ID") return "ID"
+    return str.replace(/([A-Z])/g, "_$1").toLowerCase();
+}
+
+// 下划线转换驼峰
+export const toHump = (name) => {
+    return name.replace(/\_(\w)/g, function(all, letter) {
+        return letter.toUpperCase();
+    });
+}
+
+
 /*
 @前端生成id 20位
 */
